157bc6075ca2b5cecc78bc86a745aa60fa57e0c3
ravshan77 <mygithub77@gmail.com>
2021-10-20 12:31:56 +0500

    post contents

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+const moreToggle = document.getElementById("moreToggle")
+const bottom_header = document.getElementById("bottom_headers")
+const header_bg_color = document.getElementById("header")
+const sign_icon = document.getElementById("sign_icon")
+const logo_bbc = document.getElementById("logo_bbc")
+const search_bg = document.getElementById("search_bg")
+const search_bg_input = document.getElementById("search_bg_input")
+const bg_search_i = document.getElementById("bg-search-i");
 
-function moreToggle() {
-    var element = document.getElementById("myDIV");
-    element.classList.toggle("mystyle");
-  }
 
-console.log(more);
+moreToggle.addEventListener("click", function(){
+    
+    bottom_header.classList.toggle("mystyle");
+    header_bg_color.classList.toggle("bg-black");
+    sign_icon.classList.toggle("opacity");
+    logo_bbc.classList.toggle("bg-black");
+    search_bg.classList.toggle("bg-ccc");
+    search_bg_input.classList.toggle("bg-ccc");
+    bg_search_i.classList.toggle("bg-search-i-black")
+
+})
+
+
